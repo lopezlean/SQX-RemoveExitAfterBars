bc6231e29794891dc5045a287ec94b3ca9656932
Leandro Emanuel Lopez <lopezlean@Leans-MacBook-Pro.local>
2025-12-25 16:23:51 +0100

    Fix save databanks

--- src/SettingsRemoveExitAfterBars/RemoveExitAfterBarsService.js
+++ src/SettingsRemoveExitAfterBars/RemoveExitAfterBarsService.js
@@ -1,47 +1,56 @@
-angular.module('app.settings').service('RemoveExitAfterBarsService', function($q, BackendService, AppService, L, DatabankService, SQEvents) {
+angular.module('app.settings').service('RemoveExitAfterBarsService', function ($q, BackendService, AppService, L, DatabankService, SQEvents) {
 
     function loadDatabanks() {
         DatabankService.loadVisibleDatabanks(instance.config.availableDatabanks);
     }
 
-    this.saveSettings = function() {
+    this.saveSettings = function () {
         var xmlDoc = AppService.xmlDoc;
-        
-        //databanks
-        var databanksObj = AppService.getCurrentTaskTabSettings("Databanks");
-        if(databanksObj) {
-            var databankObjs = getChildElements(databanksObj, 'Databank');
+        var settingsObj = AppService.getCurrentTaskTabSettings("RemoveExitAfterBars");
+        if (!settingsObj) return;
 
-            for(var i=0; i<databankObjs.length; i++) {
-                var databankObj = databankObjs[i];
-                var databankName = getAttrValue(databankObj, "name", "Results");
-
-                if(databankName=="Source") { databankObj.setAttribute("value", instance.config.databankSource); }
-                if(databankName=="Target") { databankObj.setAttribute("value", instance.config.databankTarget); }
-            }
-        }
+        setDatabankValue(settingsObj, "Source", instance.config.databankSource, xmlDoc);
+        setDatabankValue(settingsObj, "Target", instance.config.databankTarget, xmlDoc);
     }
-    
-    this.loadSettings = function() {
-        //databanks
-        var databanksObj = AppService.getCurrentTaskTabSettings("Databanks");
-        if(databanksObj) {
-            var databankObjs = getChildElements(databanksObj, 'Databank');
 
-            for(var i=0; i<databankObjs.length; i++) {
-                var databankObj = databankObjs[i];
+    this.loadSettings = function () {
+        var settingsObj = AppService.getCurrentTaskTabSettings("RemoveExitAfterBars");
+        if (!settingsObj) return;
 
-                var databankName = getAttrStringValue(databankObj, "name", "NA");
-                var databankValue = getAttrStringValue(databankObj, "value", "Results");
-                
-                if(databankName=="Source") { instance.config.databankSource = databankValue; }
-                if(databankName=="Target") { instance.config.databankTarget = databankValue; }
-            }
+        var databankObjs = getChildElements(settingsObj, 'Databank');
+
+        for (var i = 0; i < databankObjs.length; i++) {
+            var databankObj = databankObjs[i];
+
+            var databankName = getAttrStringValue(databankObj, "name", "NA");
+            var databankValue = getAttrStringValue(databankObj, "value", "Results");
+
+            if (databankName == "Source") { instance.config.databankSource = databankValue; }
+            if (databankName == "Target") { instance.config.databankTarget = databankValue; }
         }
-        
+
         loadDatabanks();
     }
-	
+
+    function setDatabankValue(parentObj, name, value, xmlDoc) {
+        var databankObjs = getChildElements(parentObj, 'Databank');
+        var found = false;
+        for (var i = 0; i < databankObjs.length; i++) {
+            var db = databankObjs[i];
+            if (getAttrStringValue(db, "name") == name) {
+                db.setAttribute("value", value);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            var newDb = xmlDoc.createElement("Databank");
+            newDb.setAttribute("name", name);
+            newDb.setAttribute("value", value);
+            parentObj.appendChild(newDb);
+        }
+    }
+
     var instance = this;
 
     this.config = {
@@ -50,9 +59,9 @@ angular.module('app.settings').service('RemoveExitAfterBarsService', function($q
         databankTarget: "Results",
         availableDatabanks: []
     }
-	
-    function onEvent(event, data){
-        if(event == SQEvents.get("RELOAD_DATABANKS")){
+
+    function onEvent(event, data) {
+        if (event == SQEvents.get("RELOAD_DATABANKS")) {
             loadDatabanks();
         }
     }
